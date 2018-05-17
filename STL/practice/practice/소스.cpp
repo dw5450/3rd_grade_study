9deb317baf1353b76218a1b5fe01ab1bea1684c6
dw5450 <aw13dw5450@gmail.com>
2018-05-17 09:10:43 +0900

    저장을 해보자
    5월 16일 까지의 저장

--- STL/practice/practice/소스.cpp
+++ STL/practice/practice/소스.cpp
@@ -1,60 +1,54 @@
 /////////////////////////////////////////////////////////////////////////////////////////////
 //2018.04.16		STL (월 23 , 화 56)					 7주 1
 //
-// 5장. 연관 컨테이너												정렬하는 이유, 빨리 찾기 위해서, 정렬의 기준이 되는 것이 키
-//		정렬의 기준은 <(less 연산자)
-//	set				- 키 값에 따라 정렬								기본 오름 차순을 한다/ 처음 값을 넣기는 힘들지만 찾기는 좋다. 겹치지 않아!
-//	map				- 키와 밸류로 구성, 키가 정렬의 기준			가수 이름 : 노래 등
-//	unordered_set	- 키값을 해싱하여 저장
-//	unordered_map	- 키와 밸류로 구성 , 키값을 해싱하여 밸류를 저장할 곳을 지정
-////내일 여기에서 시작합니다. Model 클레스를  mapㅇ에 넣어보자
+// 7장 반복자
+
+//
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
-#include <string>					
-#include <map>
-#include <functional>
-#include <algorithm>
-#include <vector>
+#include <iterator>
+#include <stack>
+#include <fstream>
 //다음 헤더는 실행시 지워야 할것입니다
 #include "Model.h"							//모델을 사용할시 추가후 사용해 주세요
 #include "save.h"
 #include "funcSpeedCheck.h"
-#include <fstream>
 using namespace std;
 
-//히어로와 전투력수치를 맵으로 만들어 보자
+//반복자(모든 종류의 반복자가 지원)라면 지원해야 하는 연산
+// *	: 역참조(dereferencing)
+//==	: 비교 (comaprision)
+//=		: 할당 (assignment)
 
+//반복자의 종류 (Catergory)
+//입출력반복자
+//전진반복자++;
+//전후진반복자(양방향 반복자) ++, --;
+//랜덤반복자 +=, -=;
+class MyHash {
+	public:
+		size_t operator()(const Model & a) const {
+			hash<int> hashVal;
+			
+			return hashVal(a.getSize());
+
+		}
+
+};
 int main()
 {
-	string filename = "이상한 나라의 앨리스.txt";
-	//이 파일은 UTF-8 형식의 파일로 BOM(Byte order makr)바이트가 기록되어 있다.
-	// 3바이트를 무시해야 진짜 텍스트를 읽을 수 있다.
-	ifstream in(filename);
+	//	소스.cpp를 읽고 출력
+	//	입출력자 반복자를 사용해서 작성해 봅니다.
+	//  copy 알고리즘과 삽입반복자를 사용해서 
+	// 다음시간에
 
-	if (!in) {
-		cout << filename << "을 열 수 없습니다." << endl;
-		return 0;
-	}
+	ifstream in("소스.cpp");
+	istream_iterator<char> p(in);
 
 	char c;
-	in >> c >> c >> c;		// BOM을 무시한다.
+	c = *p;
 
-	map<string, int> simap;
-
-	string s;
-	while ( in  >> s){
-		simap[s]++;
-		//cimap.operater[](c)++;
-	}
-
-	// 가장 일이가 긴 단어는? (단어는 공백으로 분리됨)
-	// STL에서 가능하면 for loop를 사용하지 않는다.(대신할 알고리즘이 있는 경우)
-	auto p = max_element(simap.begin(), simap.end(), [](const auto & p1, const auto & p2) { return p1.first.length() < p2.first.length();});
-	
-	if (p != simap.end())
-		cout << p->first << endl;
-	
-	//save("앨리스 파일에서 단어가 몇번 나오는지 찾아보자.txt");
+	//save("반복자로 cout 을 해보자.");
 
 }
--- 스크립트언어/과제 모음/test.py
+++ 스크립트언어/과제 모음/test.py
@@ -1,17 +1,23 @@
 import http.client
 import json
+import urllib
 
 
 server = "api.neople.co.kr"
 
 conn = http.client.HTTPSConnection(server)
 
-conn.request("GET", "/df/servers?apikey=9BdgXATgR7uy3XIzIaJPBHfECPoJGKlq") #서버에 GET 요청
+myApiKey = "9BdgXATgR7uy3XIzIaJPBHfECPoJGKlq"
+
+CharaterName = urllib.parse.quote("비구름마녀")
+
+conn.request("GET", "https://api.neople.co.kr/df/servers/cain/characters?characterName=" +CharaterName + "&limit=<limit>&wordType=<wordType>&apikey=" +myApiKey) #서버에 GET 요청
 req = conn.getresponse() 			#openAPI 서버에서 보내온 요청을 받아옴
 print(req.status, req.reason)
 cLen = req.getheader("Content-Length") 	#가져온 데이터 길이
 data = req.read(int(cLen))
 dict = json.loads(data)
 
-for d in dict['rows']:
-    print (d)
+print (dict['rows'][0]['characterName'])
+
+#print(dict['rows']['characterName'])
